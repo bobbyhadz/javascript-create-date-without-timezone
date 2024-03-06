c1d1af7048bf3b59e9a0b39d7f7da81d8d20be8b
bobbyhadz <opmftw@gmail.com>
2024-03-06 11:23:11 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('bobbyhadz.com');
+
+// 1) Create a Date without Timezone using JavaScript
+
+const dateStr = '2022-07-21T09:35:31.820Z';
+
+const date = new Date(dateStr);
+console.log(date); // 👉️ Thu Jul 21 2022 12:35:31 GMT+0300
+
+const result = new Date(date.toISOString().slice(0, -1));
+console.log(result); // 👉️ Thu Jul 21 2022 09:35:31 GMT+0300
